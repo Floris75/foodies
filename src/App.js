51534d7177360258c53d7aa10d88b96e619857f8
Floris75 <floris.decouflet@gmail.com>
2021-04-14 14:49:06 +0200

    resolution conflict

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import AddButton from './components/Buttons/AddButton'
+import RemoveButton from './components/Buttons/RemoveButton'
+import Instruction from './components/RecipeDetail/Instruction'
+import Illustration from './components/RecipeDetail/Illustration'
+import RecipeCard from './components/RecipeCard';
+
+
+function App() {
+  return (
+    <div></div>
+
+  );
+}
+
+export default App;
