082f65ae88c1dc3d31d7e4aee96291d4339037ca
Floris75 <floris.decouflet@gmail.com>
2021-04-14 11:45:36 +0200

    Header Version 1 terminé

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import logo from './logo.svg';
 import './App.scss';
-import Header from "./components/Header"
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <Router>
-        <Header />
-        <Switch>
-        </Switch>
-    </Router>
+    <div></div>
   );
 }
 
